a939a7a06e068c5531faf1213a58008046ecc751
Tri Rizky Shania <Shinenia26@gmail.com>
2022-02-16 22:14:35 +0700

    Latihan 9

--- Tugas 8/script.js
+++ Tugas 8/script.js
@@ -1,1 +1,46 @@
-console.log("test");
+// function ubahwarna (){
+//     title2.style.backgroundColor = 'blue'
+// }
+
+// const title2 = document.querySelector('.title2')
+// title2.onclick = ubahwarna;
+
+// title2.addEventListener('click', function(){
+//     title2.style.backgroundColor = 'lightgrey';
+// });
+
+// title2.addEventListener('click', function(){
+//     title2.style.color = 'pink';
+// });
+
+const title2 = document.querySelector('.title2');
+// title2.onclick = function() {
+//     title2.style.backgroundColor = 'lightgrey';
+// }
+
+title2.addEventListener('mouseenter', function() {
+    title2.style.color = 'red';
+});
+title2.addEventListener('mouseleave', function() {
+    title2.style.color = 'black';
+});
+
+const UbahBackground = document.getElementById('UbahBackground');
+UbahBackground.onclick = function() {
+//     document.body.style.backgroundColor = 'lightblue';
+    // document.body.setAttribute('class', 'biru-muda');
+    document.body.classList.toggle('biru-muda');
+}
+
+const title1 = document.querySelector('.title1');
+title1.addEventListener('mouseenter', function() {
+    title1.style.color = 'lightseagreen';
+});
+title1.addEventListener('mouseleave', function() {
+    title1.style.color = 'lightcoral';
+});
+
+function upperCase() {
+  const x = document.getElementById("fname");
+  x.value = x.value.toUpperCase();
+}
